8a22e3294296ab5c7207a6de46bd5c849de76617
PadoleKiran <124488404+PadoleKiran@users.noreply.github.com>
2025-12-07 17:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorSelect = document.getElementById("colorSelect");
+let btn = document.getElementById("btn");
+
+btn.onclick = function() {
+	let ind = colorSelect.selectedIndex;
+	colorSelect.remove(ind);
+}
